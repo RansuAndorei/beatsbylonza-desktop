b32a47b4eaf50533a1a1128c7156d6b54c780455
RansuAndorei <lancejuat26@gmail.com>
2022-03-20 15:25:12 +0800

    Update for the main tabs

--- src/Customers.js
+++ src/Customers.js
@@ -1,43 +1,177 @@
-import './Customers.module.css';
+import styles from './Customers.module.css';
 import {
   Link
 } from 'react-router-dom';
 
-function Customers() {
-  return (
-    <div className="container">
-      <div className="leftNavigation">
-        <div className="imageContainer">
-          <img alt="logo" src={require('./images/logo.png')} className="image"/>
-        </div>
-        <Link to="/orders">
-          <div className="tabContainer">
-              Orders
-          </div>
-        </Link>
-        <Link to="/products">
-          <div className="tabContainer">
-              Products
-          </div>
-        </Link>
-        <Link to="/customers">
-          <div className="tabContainer active">
-              Customers
-          </div>
-        </Link>
-        <Link to="/">
-          <div className="logoutContainer">
-              <div className="logout">
-                  LOGOUT
-              </div>
-          </div>
-        </Link>
-      </div>
-      <div className="contentContainer">
+import { customers } from './data';
+import { useState, useEffect } from 'react';
 
+function Customers() {
+  const [currentCustomers, setCurrentCustomers] = useState();
+  const [sortedCustomers, setSortedCustomers] = useState();
+  const [sort, setSort] = useState('date');
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(true);
+  const [searchValue, setSearchValue] = useState('');
+
+  useEffect(()=>{
+    const tempCustomers = customers.sort((a, b) =>
+      a.date > b.date ? 1 : b.date > a.date ? -1 : 0
+    )
+    setSortedCustomers(tempCustomers);
+    setCurrentCustomers(tempCustomers);
+    setLoading(false);
+  },[])
+
+  const sortFunction = (type) => {
+    setSort(type)
+    if(type === 'name'){
+      currentCustomers.sort((a, b) => `${a.firstName} ${a.middleName} ${a.lastName}` > `${b.firstName} ${b.middleName} ${b.lastName}` ? 1 : `${b.firstName} ${b.middleName} ${b.lastName}` > `${a.firstName} ${a.middleName} ${a.lastName}` ? -1 : 0)
+    }else if(type === 'address'){
+      currentCustomers.sort((a, b) => `${a.street} ${a.city} ${a.state} ${a.zipCode}` > `${b.street} ${b.city} ${b.state} ${b.zipCode}` ? 1 : `${b.street} ${b.city} ${b.state} ${b.zipCode}` > `${a.street} ${a.city} ${a.state} ${a.zipCode}` ? -1 : 0)
+    }else{
+      currentCustomers.sort((a, b) => a[type] > b[type] ? 1 : b[type] > a[type] ? -1 : 0)
+    }
+  }
+
+  const back = () =>{
+    if(page > 1){
+      setCurrentCustomers(()=>{
+        return sortedCustomers.slice((page-2)* 7, (page-1) *7)
+      })
+      setPage(prev => prev-1);
+      setSearchValue('');
+    }
+  }
+
+  const forward = () =>{
+    if(searchValue === ''){
+      if(page < Math.ceil(customers.length / 7)){
+        setCurrentCustomers(()=>{
+          return sortedCustomers.slice((page)* 7, (page+1) *7)
+        })
+        setPage(prev => prev+1);
+        setSearchValue('');
+      }
+    }else{
+      if(page < Math.ceil(currentCustomers.length / 7)){
+        setCurrentCustomers(()=>{
+          return sortedCustomers.slice((page)* 7, (page+1) *7)
+        })
+        setPage(prev => prev+1);
+        setSearchValue('');
+      }
+    }
+  }
+
+  const search = (value) =>{
+    setSearchValue(value);
+    setPage(1)
+    setCurrentCustomers(() => {
+      const tempCustomers = sortedCustomers.filter((customer) => {
+
+        if (customer.firstName.toLowerCase().includes(value, 0) || customer.middleName.toLowerCase().includes(value, 0) || customer.lastName.toLowerCase().includes(value, 0) || customer.userID.toLowerCase().includes(value, 0) || customer.street.toLowerCase().includes(value, 0) || customer.city.toLowerCase().includes(value, 0) || customer.state.toLowerCase().includes(value, 0) || customer.zipCode.toLowerCase().includes(value, 0) || customer.email.toLowerCase().includes(value, 0) || customer.contactNumber.toLowerCase().includes(value, 0)) {
+          return customer;
+        }else{
+          return null
+        }
+      });
+      return tempCustomers;
+    });
+  }
+
+  if(loading){
+    return <div>Loading</div>
+  }else{
+    return (
+      <div className={styles.container}>
+        <div className={styles.leftNavigation}>
+          <div className={styles.imageContainer}>
+            <img alt="logo" src={require('./images/logo.png')} className={styles.image}/>
+          </div>
+          <Link to="/orders">
+            <div className={styles.tabContainer}>
+                Orders
+            </div>
+          </Link>
+          <Link to="/products">
+            <div className={styles.tabContainer}>
+                Products
+            </div>
+          </Link>
+          <Link to="/customers">
+            <div className={`${styles.tabContainer} ${styles.active}`}>
+                Customers
+            </div>
+          </Link>
+          <Link to="/">
+            <div className={styles.logoutContainer}>
+                <div className={styles.logout}>
+                    LOGOUT
+                </div>
+            </div>
+          </Link>
+        </div>
+        <div className={styles.contentContainer}>
+  
+          <div className={styles.headingContainer}>
+            <div className={styles.titleContainer}>
+              Customers
+            </div>
+  
+            <div className={styles.buttonContainer}>
+  
+              <input className={styles.search} placeholder='🔎︎ Search' value={searchValue} onChange={(event)=> search(event.target.value.toLocaleLowerCase())}/>
+  
+  
+              <div className={styles.deleteContainer}>
+                Delete
+              </div>
+            </div>
+          </div>
+  
+          <div className={styles.gridContainerHeading}>
+              <div className={styles.item}></div>
+              <div className={styles.itemHeader} onClick={()=>{sortFunction('name')}}>Name  {sort === 'name' ? <span>▼</span> : <span>►</span>}</div>
+              <div className={styles.itemHeader} onClick={()=>{sortFunction('userID')}}>User ID  {sort === 'userID' ? <span>▼</span> : <span>►</span>}</div>
+              <div className={styles.itemHeader} onClick={()=>{sortFunction('address')}}>Address {sort === 'address' ? <span>▼</span> : <span>►</span>}</div>
+              <div className={styles.itemHeader} onClick={()=>{sortFunction('email')}}>Email {sort === 'email' ? <span>▼</span> : <span>►</span>}</div>
+              <div className={styles.itemHeader} onClick={()=>{sortFunction('contactNumber')}}>Contact Number  {sort === 'contactNumber' ? <span>▼</span> : <span>►</span>}</div>
+          </div>
+        
+          {
+            currentCustomers.map((customer, index)=>{
+              if(index < 7){
+                return(
+                  <div className={styles.gridContainer} key={index}>
+                    <div className={styles.item}><input type="checkbox" id={customer.id} name={customer.id} value={customer.id} /></div>
+                    <div className={styles.item}>{`${customer.firstName} ${customer.middleName} ${customer.lastName}`}</div>
+                    <div className={styles.item}>{customer.userID}</div>
+                    <div className={styles.item}>{`${customer.street} ${customer.city} ${customer.state} ${customer.zipCode}`}</div>
+                    <div className={styles.item}>{customer.email}</div>
+                    <div className={styles.item}>{customer.contactNumber}</div>
+                  </div>
+                )
+              }else{
+                return null
+              }
+            })
+          }
+
+          <div className={styles.arrowContainer}>
+            <div className={styles.arrowLeft} onClick={back}>◄</div>
+            {
+              searchValue === '' ?
+              <p>{page} of {Math.ceil(sortedCustomers.length / 7)}</p>:
+              <p>{page} of {Math.ceil(currentCustomers.length / 7)}</p>
+            }
+            <div className={styles.arrowRight}  onClick={forward}>►</div>
+          </div>
+          
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Customers;
--- src/Orders.js
+++ src/Orders.js
@@ -4,90 +4,172 @@ import {
 } from 'react-router-dom';
 
 import { orders } from './data';
+import { useState, useEffect } from 'react';
 
 function Orders() {
-  return (
-    <div className={styles.container}>
-      <div className={styles.leftNavigation}>
-        <div className={styles.imageContainer}>
-          <img alt="logo" src={require('./images/logo.png')} className={styles.image}/>
+  const [currentOrders, setCurrentOrders] = useState();
+  const [sortedOrders, setSortedOrders] = useState();
+  const [sort, setSort] = useState('date');
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(true);
+  const [searchValue, setSearchValue] = useState('');
+
+  useEffect(()=>{
+    const tempOrders = orders.sort((a, b) =>
+      a.date > b.date ? 1 : b.date > a.date ? -1 : 0
+    )
+    setSortedOrders(tempOrders);
+    setCurrentOrders(tempOrders);
+    setLoading(false);
+  },[])
+
+  const sortFunction = (type) => {
+    setSort(type)
+    currentOrders.sort((a, b) => a[type] > b[type] ? 1 : b[type] > a[type] ? -1 : 0)
+  }
+
+  const back = () =>{
+    if(page > 1){
+      setCurrentOrders(()=>{
+        return sortedOrders.slice((page-2)* 7, (page-1) *7)
+      })
+      setPage(prev => prev-1);
+      setSearchValue('');
+    }
+  }
+
+  const forward = () =>{
+    if(searchValue === ''){
+      if(page < Math.ceil(orders.length / 7)){
+        setCurrentOrders(()=>{
+          return sortedOrders.slice((page)* 7, (page+1) *7)
+        })
+        setPage(prev => prev+1);
+        setSearchValue('');
+      }
+    }else{
+      if(page < Math.ceil(currentOrders.length / 7)){
+        setCurrentOrders(()=>{
+          return sortedOrders.slice((page)* 7, (page+1) *7)
+        })
+        setPage(prev => prev+1);
+        setSearchValue('');
+      }
+    }
+  }
+
+  const search = (value) =>{
+    setSearchValue(value);
+    setPage(1)
+    setCurrentOrders(() => {
+      const tempOrders = sortedOrders.filter((order) => {
+
+        if (order.date.toLowerCase().includes(value, 0) || order.productID.toLowerCase().includes(value, 0) || order.orderID.toLowerCase().includes(value, 0) || order.customerID.toLowerCase().includes(value, 0) || order.address.toLowerCase().includes(value, 0) || order.status.toLowerCase().includes(value, 0)) {
+          return order;
+        }else{
+          return null
+        }
+      });
+      return tempOrders;
+    });
+  }
+
+  if(loading){
+    return <div>Loading</div>
+  }else{
+    return (
+      <div className={styles.container}>
+        <div className={styles.leftNavigation}>
+          <div className={styles.imageContainer}>
+            <img alt="logo" src={require('./images/logo.png')} className={styles.image}/>
+          </div>
+          <Link to="/orders">
+            <div className={`${styles.tabContainer} ${styles.active}`}>
+                Orders
+            </div>
+          </Link>
+          <Link to="/products">
+            <div className={styles.tabContainer}>
+                Products
+            </div>
+          </Link>
+          <Link to="/customers">
+            <div className={styles.tabContainer}>
+                Customers
+            </div>
+          </Link>
+          <Link to="/">
+            <div className={styles.logoutContainer}>
+                <div className={styles.logout}>
+                    LOGOUT
+                </div>
+            </div>
+          </Link>
         </div>
-        <Link to="/orders">
-          <div className={`${styles.tabContainer} ${styles.active}`}>
+        <div className={styles.contentContainer}>
+  
+          <div className={styles.headingContainer}>
+            <div className={styles.titleContainer}>
               Orders
-          </div>
-        </Link>
-        <Link to="/products">
-          <div className={styles.tabContainer}>
-              Products
-          </div>
-        </Link>
-        <Link to="/customers">
-          <div className={styles.tabContainer}>
-              Customers
-          </div>
-        </Link>
-        <Link to="/">
-          <div className={styles.logoutContainer}>
-              <div className={styles.logout}>
-                  LOGOUT
+            </div>
+  
+            <div className={styles.buttonContainer}>
+  
+              <input className={styles.search} placeholder='🔎︎ Search' value={searchValue} onChange={(event)=> search(event.target.value.toLocaleLowerCase())}/>
+  
+  
+              <div className={styles.deleteContainer}>
+                Delete
               </div>
-          </div>
-        </Link>
-      </div>
-      <div className={styles.contentContainer}>
-
-        <div className={styles.headingContainer}>
-          <div className={styles.titleContainer}>
-            Orders
-          </div>
-
-          <div className={styles.buttonContainer}>
-
-              <input className={styles.search} placeholder='🔎︎ Search' />
-
-
-            <div className={styles.deleteContainer}>
-              Delete
             </div>
           </div>
-        </div>
-
-        <div className={styles.gridContainerHeading}>
-            <div className={styles.item}></div>
-            <div className={styles.item}>Date</div>
-            <div className={styles.item}>Product ID</div>
-            <div className={styles.item}>Order ID</div>
-            <div className={styles.item}>Quantity</div>
-            <div className={styles.item}>Address</div>
-            <div className={styles.item}>Status</div>
-        </div>
-      
-        {
-          orders.map((order, index)=>{
-            return(
-              <div className={styles.gridContainer} key={index}>
-                <div className={styles.item}><input type="checkbox" id={order.id} name={order.id} value={order.id} /></div>
-                <div className={styles.item}>{order.date}</div>
-                <div className={styles.item}>{order.productID}</div>
-                <div className={styles.item}>{order.orderID}</div>
-                <div className={styles.item}>{order.quantity}</div>
-                <div className={styles.item}>{order.address}</div>
-                <div className={styles.item}>{order.status}</div>
-              </div>
-            )
-          })
-        }
-
-        <div className={styles.arrowContainer}>
-          <div className={styles.arrowLeft}>◄</div>
-          <p>1 of 2</p>
-          <div className={styles.arrowRight}>►</div>
-        </div>
+  
+          <div className={styles.gridContainerHeading}>
+              <div className={styles.item}></div>
+              <div className={styles.itemHeader} onClick={()=>{sortFunction('date')}}>Date  {sort === 'date' ? <span>▼</span> : <span>►</span>}</div>
+              <div className={styles.itemHeader} onClick={()=>{sortFunction('productID')}}>Product ID  {sort === 'productID' ? <span>▼</span> : <span>►</span>}</div>
+              <div className={styles.itemHeader} onClick={()=>{sortFunction('orderID')}}>Order ID  {sort === 'orderID' ? <span>▼</span> : <span>►</span>}</div>
+              <div className={styles.itemHeader} onClick={()=>{sortFunction('customerID')}}>Customer ID  {sort === 'customerID' ? <span>▼</span> : <span>►</span>}</div>
+              <div className={styles.itemHeader} onClick={()=>{sortFunction('quantity')}}>Quantity  {sort === 'quantity' ? <span>▼</span> : <span>►</span>}</div>
+              <div className={styles.itemHeader} onClick={()=>{sortFunction('address')}}>Address  {sort === 'address' ? <span>▼</span> : <span>►</span>}</div>
+              <div className={styles.itemHeader} onClick={()=>{sortFunction('status')}}>Status  {sort === 'status' ? <span>▼</span> : <span>►</span>}</div>
+          </div>
         
+          {
+            currentOrders.map((order, index)=>{
+              if(index < 7){
+                return(
+                  <div className={styles.gridContainer} key={index}>
+                    <div className={styles.item}><input type="checkbox" id={order.id} name={order.id} value={order.id} /></div>
+                    <div className={styles.item}>{order.date}</div>
+                    <div className={styles.item}>{order.productID}</div>
+                    <div className={styles.item}>{order.orderID}</div>
+                    <div className={styles.item}>{order.customerID}</div>
+                    <div className={styles.item}>{order.quantity}</div>
+                    <div className={styles.item}>{order.address}</div>
+                    <div className={styles.item}>{order.status}</div>
+                  </div>
+                )
+              }else{
+                return null
+              }
+            })
+          }
+
+          <div className={styles.arrowContainer}>
+            <div className={styles.arrowLeft} onClick={back}>◄</div>
+            {
+              searchValue === '' ?
+              <p>{page} of {Math.ceil(sortedOrders.length / 7)}</p>:
+              <p>{page} of {Math.ceil(currentOrders.length / 7)}</p>
+            }
+            <div className={styles.arrowRight}  onClick={forward}>►</div>
+          </div>
+          
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Orders;
